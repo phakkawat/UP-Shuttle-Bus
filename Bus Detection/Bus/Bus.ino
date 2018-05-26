09f32ff0e3d942813305db5ec82d66e6dee07058
phakkawat <39500213+phakkawat@users.noreply.github.com>
2018-05-26 11:29:19 +0700

    Add files via upload

---
+++ Bus Detection/Bus/Bus.ino
@@ -0,0 +1,157 @@
+// Include Libary.
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+
+// Set NRFname and CE,CSN pin Name(CE, CSN).
+RF24 radio(8, 7);
+// Set NFR beginning node.
+byte addresses[][6] = {"0Node", "1Node"};
+// Creat integer value.
+int Station, BusNumRe, Pass, Count, WaitTime, CH = 0;
+int SendPKY = 1, SendCE = 1, SendSCI = 1, SendEN = 1, SendICT = 1;
+
+int BusNum = 2;      // Bus Number
+
+// Function of data sending.
+void SendNum(int CH)
+{
+  radio.setChannel(CH);
+  do
+  {
+    for (Count = 0; Count <= 1; Count++ )
+    {
+      radio.stopListening();
+      radio.write(&BusNum, sizeof(BusNum));
+      Serial.print("Send : ");
+      Serial.print(BusNum);
+      Serial.print("  ");
+      delay(200);
+      radio.startListening();
+
+      delay(500);
+
+      for (WaitTime = 0 ; WaitTime < 3 ; WaitTime++)
+      {
+        if (radio.available())
+        {
+          radio.read(&BusNumRe, sizeof(BusNumRe));
+          radio.stopListening();
+          Serial.print("\nRecieve : ");
+          Serial.print(BusNumRe);
+          Serial.println("\n");
+
+          if (BusNum != BusNumRe)
+          {
+            Count = 0;
+            WaitTime = 4;
+          }
+          else
+          {
+            Pass = 1;
+            WaitTime = 4;
+          }
+        }
+        else delay(1000);
+      }
+      Count = Count + 1;
+    }
+    WaitTime = 0;
+    delay(1000);
+  } while (Pass != 1);
+  delay(500);
+}
+
+// Function of clearing values.
+void FuncClear()
+{
+  Pass = 0;
+  BusNumRe = 0;
+  Count = 0;
+}
+
+void setup()
+{
+  // Set Serial at 115200 baud.
+  Serial.begin(115200);
+  // Set NRF write node, read node, Channel, Datarate, Power, Status.
+  radio.begin();
+  radio.openWritingPipe(addresses[1]); // Tx.
+  radio.openReadingPipe(1, addresses[0]); // Rx.
+  radio.setChannel(10);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.startListening();
+}
+
+// Function of main loop.
+void loop()
+{
+  for (Station = 0; Station < 4; Station++)
+  {
+    Serial.println(Station);        // Print station number.
+    do
+    {
+      delay(1000);                  // Set delay before connect to station.
+      if (Station == 0)             // Check station number.
+      {
+        Serial.print("Station : PKY");      // Print station name.
+        CH = 120;                    // Set Bus Channel.
+        Serial.print(" --- Channel : ");
+        Serial.println(CH);
+        Serial.print("Send to PKY : ");
+        Serial.println(SendPKY);
+        SendPKY++;
+      }
+      else if (Station == 1)        // Check station number.
+      {
+        Serial.print("Station : CE");       // Print station name.
+        CH = 100;                    // Set Bus Channel.
+        Serial.print(" --- Channel : ");
+        Serial.println(CH);
+        Serial.print("Send to CE : ");
+        Serial.println(SendCE);
+        SendCE++;
+      }
+      else if (Station == 2)        // Check station number.
+      {
+        Serial.print("Station : SCI");      // Print station name.
+        CH = 80;                    // Set Bus Channel.
+        Serial.print(" --- Channel : ");
+        Serial.println(CH);
+        Serial.print("Send to SCI : ");
+        Serial.println(SendSCI);
+        SendSCI++;
+      }
+      else if (Station == 3)        // Check station number.
+      {
+        if (BusNum % 2 == 0)        // Check Bus number if it's even number do function of EN station.
+        {
+          Serial.print("Station : EN");     // Print station name.
+          CH = 60;                  // Set Bus Channel.
+          Serial.print(" --- Channel : ");
+          Serial.println(CH);
+          Serial.print("Send to EN : ");
+          Serial.println(SendEN);
+          SendEN++;
+        }
+        else
+        {
+          Serial.print("Station : ICT");    // Print station name.
+          CH = 40;                  // Set Bus Channel.
+          Serial.print(" --- Channel : ");
+          Serial.println(CH);
+          Serial.print("Send to ICT : ");
+          Serial.println(SendICT);
+          SendICT++;
+        }
+      }
+      SendNum(CH);                    // Do function of data sending.
+      delay(10000);                 // Waiting for next station transmission.
+    } while (Pass == 0);
+    FuncClear();                    // Clear values.
+  }
+  BusNum = BusNum+2;
+}
---
+++ Bus Detection/Station_SCI/Station_SCI.ino
@@ -0,0 +1,220 @@
+//*********************************************Espresso***************************************************************
+#include <ESP8266WiFi.h>
+#include <MicroGear.h>
+
+const char* ssid     = "Test";
+const char* password = "123456780";
+
+#define APPID   "UPshuttlebus"
+#define KEY     "beUIYcyNimrRb40"
+#define SECRET  "YuuToXm7FeraLdstDC3Agw3gj"
+#define ALIAS   "Station02"
+#define TargetWeb "HTMLMonitor02"
+
+WiFiClient client;
+
+int timer = 0, CountSend = 1, OE = 0;
+
+//**************************************************NRF***************************************************************
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+
+RF24 radio(15, 2);
+byte addresses[][6] = {"0Node", "1Node"};
+
+int BusNum = 0;
+
+void FuncClear()
+{
+  BusNum = 0;
+  timer = 0;
+  OE = 0;
+}
+
+//**********************************************Espresso***************************************************************
+MicroGear microgear(client);
+
+void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) {
+  Serial.print("Incoming message --> ");
+  msg[msglen] = '\0';
+  Serial.println((char *)msg);
+}
+void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
+  Serial.println("Connected to NETPIE...");
+  microgear.setAlias(ALIAS);
+}
+
+void toNetpie(int BusNum)
+{
+  radio.write(&BusNum, sizeof(BusNum));
+  delay(1000);
+  radio.write(&BusNum, sizeof(BusNum));
+  Serial.print("Send to Bus : ");
+  Serial.println(BusNum);
+
+  String str = "SCI";
+
+  str += ":" + String(BusNum);
+
+  String data;
+
+  OE = BusNum % 2;
+  if (OE == 0)
+  {
+    data = "/" + String(BusNum) + "/E0";
+  }
+  else
+  {
+    data = "/" + String(BusNum) + "/O0";
+  }
+
+  char msg[256];
+  data.toCharArray(msg, data.length());
+
+  if (microgear.connected())
+  {
+    microgear.connect(APPID);
+    Serial.println("connected");
+    microgear.loop();
+    microgear.writeFeed("UPshuttlebus", str, "69K4sOltXlCxTff9ukvKqJLmmy0iAuXd");      // Send data to feed
+    delay(200);
+    microgear.chat(TargetWeb, msg);
+    Serial.print("Message to Web : ");
+    Serial.println(msg);
+    Serial.println(str);
+    Serial.print("Data is Send : ");
+    Serial.println(BusNum);
+    delay(1000);
+    Serial.print("Mission Success ");
+    Serial.println(CountSend);
+    Serial.print("\n");
+    CountSend++;
+  }
+  else
+  {
+    Serial.println("connection lost, reconnect...");
+    microgear.connect(APPID);
+    if (microgear.connected())
+    {
+      Serial.println("connected");
+      microgear.writeFeed("UPshuttlebus", str, "69K4sOltXlCxTff9ukvKqJLmmy0iAuXd");      // Send data to feed
+      delay(200);
+      microgear.chat(TargetWeb, msg);
+      Serial.print("Message to Web : ");
+      Serial.println(msg);
+      Serial.println(str);
+      Serial.print("Data is Send : ");
+      Serial.println(BusNum);
+      delay(1000);
+      Serial.print("Mission Success ");
+      Serial.println(CountSend);
+      Serial.print("\n");
+      CountSend++;
+    }
+  }
+  delay(100);
+}
+
+
+void setup()
+{
+  //******************************************************NRF*************************************************************************************
+  Serial.begin(115200);
+  radio.begin();
+  radio.openWritingPipe(addresses[0]);      // (Tx) = radio.openWritingPipe("0Node")
+  radio.openReadingPipe(1, addresses[1]);     // Rx
+  radio.setChannel(80);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.stopListening();
+
+  delay(100);
+
+  Serial.println("Ready...");
+
+  //*****************************************************Espresso**********************************************************************************
+  microgear.on(MESSAGE, onMsghandler);
+  microgear.on(CONNECTED, onConnected);
+
+  Serial.println("Starting...");
+
+  if (WiFi.begin(ssid, password))
+  {
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  }
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  microgear.init(KEY, SECRET, ALIAS);
+
+  microgear.connect(APPID);
+
+  Serial.println("Reciever SCI\n");
+}
+
+
+
+void loop()
+{
+  for (timer = 0 ; timer < 600 ; timer++)     //Reconnect Wi-Fi every 10 Minutes
+  {
+    radio.startListening();     // nRF24L01n ready to recieve the value
+    if (radio.available())
+    {
+      radio.read(&BusNum, sizeof(BusNum));    // Recieve bus number from transmission device.
+      Serial.print("Recieve : ");
+      Serial.print(BusNum);
+      Serial.print("  ");
+
+      delay(200);
+
+      radio.stopListening();      // nRF24L01n not ready to recieve the value
+      delay(500);
+
+      Serial.println("************Sent Data***************");
+      toNetpie(BusNum);     // Send value to NETPIE.
+      FuncClear();    // Clear all values.
+
+      delay(15000);
+      Serial.println("Ready...");
+    }
+
+    if (timer == 180)
+    {
+      microgear.chat(TargetWeb, "/00");
+    }
+
+    delay(1000);
+  }
+
+  WiFi.disconnect();      // Disconnect WiFi
+  delay(200);
+
+  if (WiFi.begin(ssid, password)) // Reconnect WiFi
+  {
+    while (WiFi.status() != WL_CONNECTED)
+    {
+      delay(500);
+      Serial.print(".");
+    }
+  }
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  microgear.init(KEY, SECRET, ALIAS);
+
+  microgear.connect(APPID);   // Connect to NETPIE
+
+  Serial.println("Reciever SCI\n");
+
+  timer = 0;
+}
